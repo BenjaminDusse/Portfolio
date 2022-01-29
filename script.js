c047055b1bda732fd4996e69a348969811441624
BenjaminDusse <78902387+BenjaminDusse@users.noreply.github.com>
2022-01-30 01:11:06 +0500

    Setted correctly

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('Its working')
 let theme = localStorage.getItem('theme')
 
 if(theme == null){
-	setTheme('light')
+	setTheme('blue')
 }else{
 	setTheme(theme)
 }
@@ -38,3 +38,4 @@ function setTheme(mode){
 
 	localStorage.setItem('theme', mode)
 }
+
